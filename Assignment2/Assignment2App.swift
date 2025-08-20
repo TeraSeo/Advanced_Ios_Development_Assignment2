6601bb3b82912d0ff4dfc3fefb7126ca7b8dc4d6
TeraSeo <seotj0413@gmail.com>
2025-08-20 15:17:04 +0900

    Initial Commit

---
+++ Assignment2/Assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2App.swift
+//  Assignment2
+//
+//  Created by 서태준 on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment2
+//
+//  Created by 서태준 on 8/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
